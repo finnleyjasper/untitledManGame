f8050c37b564af337028a2137f3dd0014ba12289
DavidJPhillips <163076698+DavidJPhillips@users.noreply.github.com>
2024-05-14 12:05:48 +1000

    Revert "Dav tilemap and deco"

--- MrFrogMsToad/Assets/Scripts/Player/Player.cs
+++ MrFrogMsToad/Assets/Scripts/Player/Player.cs
@@ -1,61 +1,22 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerMovement))]
-
 public class Player : MonoBehaviour
 {
-    public PlayerMovement movement { get; private set; }
-    [SerializeField] private Collider2D collider;
-
-    public KeyCode up;
-    public KeyCode down;
-    public KeyCode left;
-    public KeyCode right;
-
     public int _score;
     public int _lives;
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        this.movement = GetComponent<PlayerMovement>();
-        this.collider = GetComponent<Collider2D>();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        ChangeDirection();
-    }
-
-
-    private void ChangeDirection()
-    {
-        if (Input.GetKeyDown(up))
-        {
-            this.movement.SetDirection(Vector2.up);
-        }
-        if (Input.GetKeyDown(down))
-        {
-            this.movement.SetDirection(Vector2.down);
-        }
-        if (Input.GetKeyDown(left))
-        {
-            this.movement.SetDirection(Vector2.left);
-        }
-        if (Input.GetKeyDown(right))
-        {
-            this.movement.SetDirection(Vector2.right);
-        }
-
-        ChangeOrientation();
-    }
-
-    private void ChangeOrientation()
-    {
-        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
-        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+        
     }
 
     public int Score
@@ -82,8 +43,5 @@ public class Player : MonoBehaviour
         }
     }
 
-    public static explicit operator Player(GameObject v)
-    {
-        throw new NotImplementedException();
-    }
+
 }
